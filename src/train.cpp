61db4640646fa8adf17a373c29fd2fa3e75317f3
Denis Suchkov <99133137+PlagueDoctoh@users.noreply.github.com>
2022-05-07 21:56:47 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -7,3 +7,51 @@ Train::Cage* Train::create(bool light) {
     cage->prev = nullptr;
     return cage;
 }
+
+void Train::addCage(bool light) {
+    if (last && first) {
+        last->next = create(light);
+        last->next->prev = last;
+        last = last->next;
+        first->prev = last;
+        last->next = first;
+    } else { 
+        first = create(light);
+        last = first;
+    }
+}
+
+int Train::getLength() {
+    int len = 0;
+    first->light = true;
+    Cage* temp = first;
+    while (true) {
+        len = len + 1;
+        for (int j = 0; j < len; j++) {
+            temp = temp->next;
+            countOp = countOp + 1;
+        }
+        if (temp->light) {
+            temp->light = false;
+            for (int j = 0; j < len; j++) {
+                temp = temp->prev;
+                countop++;
+            }
+        } else {
+            while (!temp->light) {
+                len = len+1;
+                temp = temp->next;
+                countOp = countOp + 1;
+            }
+            temp->light = false;
+            for (int j = 0; j < len; j+ ) {
+                temp = temp->prev;
+                countOp=countO+1;
+            }
+        }
+        if (!temp->light) {
+            break;
+        }
+    }
+    return len;
+}
