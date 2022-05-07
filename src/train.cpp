25ce9bd5c70c66c7f3c02548347cf9109c1ad2e9
Denis Suchkov <99133137+PlagueDoctoh@users.noreply.github.com>
2022-05-07 21:45:29 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,9 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+Train::Cage* Train::create(bool light) {
+    Cage* cage = new Cage;
+    cage->light = light;
+    cage->next = nullptr;
+    cage->prev = nullptr;
+    return cage;
+}
